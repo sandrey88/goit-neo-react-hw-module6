fcc85a2063c866a57654013931697775134c8dc1
Andrii <88sushilnikov@gmail.com>
2025-01-10 15:18:27 +0200

    version 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
-import { useSelector } from 'react-redux';
 import ContactForm from './components/ContactForm/ContactForm';
 import SearchBox from './components/SearchBox/SearchBox';
 import ContactList from './components/ContactList/ContactList';
-import { selectContacts } from './redux/contactsSlice';
-import { selectNameFilter } from './redux/filtersSlice';
 import './App.css';
 
 const App = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
-
-  const getVisibleContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  };
-
-  const visibleContacts = getVisibleContacts();
-
   return (
     <div className="container">
       <h1>Phonebook</h1>
       <ContactForm />
       <SearchBox />
-      <ContactList contacts={visibleContacts} />
+      <ContactList />
     </div>
   );
 };
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,25 @@
+import { useSelector } from 'react-redux';
 import Contact from '../Contact/Contact';
+import { selectContacts } from '../../redux/contactsSlice';
+import { selectNameFilter } from '../../redux/filtersSlice';
 import css from './ContactList.module.css';
 
-const ContactList = ({ contacts }) => {
+const ContactList = () => {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const visibleContacts = getVisibleContacts();
+
   return (
     <div className={css.list}>
-      {contacts.map(contact => (
+      {visibleContacts.map(contact => (
         <Contact
           key={contact.id}
           contact={contact}
